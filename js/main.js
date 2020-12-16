11bca4863de1a4dbb595909cb4157d4782e47f3b
Riccardo Ferrari <richi.f93@gmail.com>
2020-12-16 15:59:52 +0100

    v-if farle comparire tutte in una volta

--- js/main.js
+++ js/main.js
@@ -9,30 +9,21 @@ var app = new Vue({
 
   methods: {
 
-
-
-
-
   },
   mounted: function() {
     var self = this;
     for (var i = 0; i < 10; i++) {
 
       axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(function (result) {
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(function (result) {
 
+        var randomMail = result.data.response
+        self.mails.push(randomMail)
 
+      });
 
+    };
 
-
-          var randomMail = result.data.response
-          self.mails.push(randomMail)
-
-        });
-
-    }
-
-
-    }
-  });
+  }
+});
